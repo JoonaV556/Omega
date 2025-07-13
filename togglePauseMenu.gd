ca81c3b65ec82a821a498a9d32fba16eb8c95e39
joonav-laptop <joona.virmajoki@gmail.com>
2025-07-14 01:21:35 +0300

    pause menu can be toggled with esc key

--- togglePauseMenu.gd
+++ togglePauseMenu.gd
@@ -1,7 +1,21 @@
 extends CanvasLayer
 
+# makes the property visible in inspector and allows assigning it in editor
+@export var pauseMenuNode: CanvasLayer
+
+# _process is called by engine once every frame, practically an update function for gdscripts
 func _process(delta):
+	# checks if input action named "TogglePauseMenu" in input maps is pressed this frame
 	if Input.is_action_just_released("TogglePauseMenu"):
 		_toggle()
 func _toggle():
-	print("toggled pause menu")
+	if pauseMenuNode.is_visible():
+		pauseMenuNode.hide()
+	else:
+		pauseMenuNode.show()
+
+# visible [default: true]set_visible(value) setteris_visible() getter
+
+# ● void hide()
+
+# Hides any CanvasItem under this CanvasLayer. This
