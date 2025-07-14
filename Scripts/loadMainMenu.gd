1fcda99fd78d9e2b1c12fde5356dbcb98825f92a
joonav-laptop <joona.virmajoki@gmail.com>
2025-07-14 16:12:37 +0300

    added looping background audio ambience to game scene

--- Scripts/loadMainMenu.gd
+++ Scripts/loadMainMenu.gd
@@ -1,4 +1,4 @@
 extends Button
 
 func loadMainMenu():
-	get_tree().change_scene_to_file("res://MainMenu.tscn")
+	get_tree().change_scene_to_file("res://Levels/MainMenu.tscn")
--- Scripts/startGame.gd
+++ Scripts/startGame.gd
@@ -1,4 +1,4 @@
 extends Button
 
 func _startgame():
-	get_tree().change_scene_to_file("res://Game.tscn")
+	get_tree().change_scene_to_file("res://Levels/Game.tscn")
