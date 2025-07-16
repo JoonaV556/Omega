48b014b82a1f3459c3c74de08fbeb49215086f58
Oliver Leppänen <oliverleppanen@gmail.com>
2025-07-16 15:29:43 +0300

    Create ptg_house folder and build house out of floor scenes

--- Scripts/floor.gd
+++ Scripts/floor.gd
@@ -1,8 +1,14 @@
 class_name Floor
 extends Node2D
 
+@export var collision_layer: int = 1
+
 func _ready():
 	for node in get_children():
+		if node is CollisionObject2D:
+			node.set_collision_layer(collision_layer)
+			node.set_collision_mask(collision_layer)
+			
 		if not node.has_meta("is_player"):
 			continue
 		show()
--- Scripts/floor_switch_trigger.gd
+++ Scripts/floor_switch_trigger.gd
@@ -5,12 +5,13 @@ extends Area2D
 @export var hide_floors: Array[Node2D]
 
 func _on_body_entered(body: Node2D) -> void:
-	print(target_floor)
 	if target_floor == null:
 		return
-	print(body)
 	
 	body.reparent(target_floor)
+	if body is CollisionObject2D:
+		body.set_collision_layer(target_floor.collision_layer)
+		body.set_collision_mask(target_floor.collision_layer)
 	target_floor.show()
 	for floor in hide_floors:
 		floor.hide()
