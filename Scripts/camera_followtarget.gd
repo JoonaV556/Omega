a984f20a2a47a5cf3f8787d84aff1e012135e19b
joonav-laptop <joona.virmajoki@gmail.com>
2025-07-16 22:48:50 +0300

    setup 2floor ambient light with CanvasModulate node setup desk lamp light in 2floor, small adjustments to Player scene for lights and shadows,

--- Scripts/camera_followtarget.gd
+++ Scripts/camera_followtarget.gd
@@ -2,4 +2,5 @@ extends Camera2D
 # Makes the camera follow the given target object on screen
 @export var target: Node2D
 func _process(delta: float) -> void:
-	position = target.position
+	if target != null:
+		position = target.position
