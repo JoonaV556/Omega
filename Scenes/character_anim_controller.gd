097aec0045b3742cb3efcb656515495f9b97543c
JoonaV556 <joona.virmajoki@gmail.com>
2025-07-20 02:58:30 +0300

    added naked sprite anims to player scene

---
+++ Scenes/character_anim_controller.gd
@@ -0,0 +1,45 @@
+extends AnimatedSprite2D
+@export var character_body: CharacterBody2D
+enum look_dir {down, up, left, right}
+var current_look_dir = look_dir.down
+
+func _process(delta: float) -> void:
+	if character_body == null:
+		return
+	var velocity = character_body.get_real_velocity()
+	var magnitude = velocity.length()
+	print("Velocity: " + str(velocity))
+	print("Velocity magnitude: " + str(magnitude))
+	var moving = magnitude > 1.0
+	#up
+	if (velocity.x > -1.0 and velocity.x < 1.0) and velocity.y < -98.0:
+		current_look_dir = look_dir.up
+	#down
+	if (velocity.x > -1.0 and velocity.x < 1.0) and velocity.y > 98.0:
+		current_look_dir = look_dir.down
+	#left
+	if velocity.x < -65.0:
+		current_look_dir = look_dir.left
+	#right
+	if velocity.x > 65.0:
+		current_look_dir = look_dir.right
+	if moving:
+		match current_look_dir:
+			look_dir.up:
+				play("Walk_Up")
+			look_dir.down:
+				play("Walk_Down")
+			look_dir.left:
+				play("Walk_Left")
+			look_dir.right:
+				play("Walk_Right")
+	else:
+		match current_look_dir:
+			look_dir.up:
+				play("IdleBreathing_Up")
+			look_dir.down:
+				play("IdleBreathing_Down")
+			look_dir.left:
+				play("IdleBreathing_Left")
+			look_dir.right:
+				play("IdleBreathing_Right")
