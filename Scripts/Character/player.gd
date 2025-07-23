068f063c0987749eabda51332981bd1357be9e80
Oliver Leppänen <oliverleppanen@gmail.com>
2025-07-23 23:23:39 +0300

    Implement basic journal functionality

--- Scripts/Character/player.gd
+++ Scripts/Character/player.gd
@@ -7,9 +7,11 @@ const Shell = preload("res://Scenes/Inventory/Items/Shell.tscn")
 @export var speed = 400
 
 var inventory: Inventory
+var journal: Journal
 
 func _ready():
 	inventory = Inventory.new()
+	journal = Journal.new()
 
 func update_character_physics():
 	var input_direction = Input.get_vector("MoveLeft", "MoveRight", "MoveUp", "MoveDown")
@@ -18,3 +20,5 @@ func update_character_physics():
 func _input(event):
 	if Input.is_action_just_released("Open Inventory"):
 		GlobalInventoryHandler.open_single_inventory(inventory)
+	if Input.is_action_just_released("Open Journal"):
+		GlobalJournalHandler.open_journal(journal)
---
+++ Scripts/Journal/journal_ui.gd
@@ -0,0 +1,73 @@
+@tool
+class_name JournalUI
+extends CanvasLayer
+
+@onready var quest_titles: ItemList = $Background/MarginContainer/PanelContainer/HSplitContainer/QuestTitleContainer
+@onready var quest_notes: VBoxContainer = $Background/MarginContainer/PanelContainer/HSplitContainer/ScrollContainer/QuestNoteContainer
+
+var journal: Journal
+
+func _is_debug():
+	return self == get_tree().current_scene
+
+func _ready():
+	quest_titles.item_clicked.connect(_on_quest_selected)
+	
+	if not Engine.is_editor_hint() and _is_debug():
+		var test_journal = Journal.new()
+		var test_quest1 = Quest.new("Fuck around")
+		test_quest1.add_note(Note.new("Maybe you could try fucking around?"))
+		var test_quest2 = Quest.new("Find out")
+		test_quest2.add_note(Note.new("You set out ot find out"))
+		test_quest2.add_note(Note.new("You found out..."))
+		
+		test_journal.add_quest(test_quest1)
+		test_journal.add_quest(test_quest2)
+		
+		load_journal(test_journal)
+	
+func load_journal(journal_to_load: Journal):
+	_clear()
+	
+	journal = journal_to_load
+	
+	var quest_title_node
+	for quest in journal.quests:
+		quest_titles.add_item(quest.title)
+
+func _on_quest_selected(index: int, at_position: Vector2, mouse_button_index: int) -> void:
+	if mouse_button_index != MOUSE_BUTTON_LEFT: return
+	_load_quest_notes_by_index(index, journal)
+
+func _load_quest_notes_by_index(index: int, journal: Journal):
+	if journal == null or journal.quests.size()-1 < index: return
+	var notes = journal.quests[index].notes
+	
+	_clear_quest_notes()
+	for note in notes:
+		_add_quest_note(note)
+	
+func _add_quest_note(note: Note):
+	var note_label = Label.new()
+	quest_notes.add_child(note_label)
+	note_label.text = note.text
+
+func _clear_quest_titles():
+	for index in range(quest_titles.item_count):
+		quest_titles.remove_item(index)
+
+func _clear_quest_notes():
+	for child in quest_notes.get_children():
+		quest_notes.remove_child(child)
+
+func _clear():
+	_clear_quest_titles()
+	_clear_quest_notes()
+
+func _close():
+	hide()
+	_clear()
+
+func _input(event):
+	if Input.is_action_just_released("Close Journal"):
+		_close()
