4632cd45f9a589dedbd48d35c18008a56b84b0d3
JoonaV.KubuntuDesktop <joona.virmajoki@gmail.com>
2026-04-07 01:06:16 +0300

    add bullet brass casing drop sfx

---
+++ Code/Other/CombatSample/global_events_scene_proxy.gd
@@ -0,0 +1,10 @@
+class_name GESceneProxy
+extends Node
+
+signal gesp_on_bullet_landed(pos)
+
+func _ready():
+    GlobalEventBus.on_bullet_landed.connect(geb_on_bullet_land)
+
+func geb_on_bullet_land(_pos):
+    gesp_on_bullet_landed.emit(_pos)
