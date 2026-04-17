eb0881bb2ec22ee46c3f291825fdeac91f044518
JoonaV.KubuntuDesktop <joona.virmajoki@gmail.com>
2026-04-17 23:44:56 +0300

    add npc pursue target task

--- Code/Other/NPCSample/target_selector.gd
+++ Code/Other/NPCSample/target_selector.gd
@@ -6,6 +6,9 @@ extends Node
 
 var target: Node2D
 
+@export var _has_target: bool = false
+@export var target_pos: Vector2
+
 func _ready() -> void:
 	observer.on_detected_updated.connect(on_target_candidates_updated)
 	observer.on_undetected.connect(_on_undetected)
@@ -20,19 +23,27 @@ func on_target_candidates_updated(candidates: Dictionary):
 
 func select(_target: Node2D):
 	target = _target
+	_has_target = true
 
 func _on_undetected(obs: Observable):
 	# forget target if it was undetected
 	if target == obs:
 		target = null
+		_has_target = false
 	# try select new one
 	try_reselect()
 
 func _process(delta: float) -> void:
 	if !target:
 		try_reselect()
+		target_pos = Vector2.ZERO
+	else:
+		target_pos = target.global_position
 
 func try_reselect():
 	if observer.detected.size() > 0:
 		if observer.detected.keys()[0] != null:
 			select(observer.detected.keys()[0])
+
+func has_target() -> bool:
+	return _has_target
