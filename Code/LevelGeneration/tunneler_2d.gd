792d54154f1d59435a5333318180c65b05a30eff
JoonaV.KubuntuDesktop <joona.virmajoki@gmail.com>
2026-07-08 21:12:28 +0300

    - refactor: tunneler direction picking behavior

--- Code/LevelGeneration/tunneler_2d.gd
+++ Code/LevelGeneration/tunneler_2d.gd
@@ -6,17 +6,25 @@ enum move_direction {N, S, E, W}
 var _bounds_min : Vector2i
 var _bounds_max : Vector2i
 
-var position
+var position : Vector2i
+
+var directions : Dictionary[move_direction, Vector2i] = {
+	move_direction.N: Vector2i(0, -1),
+	move_direction.S: Vector2i(0, 1),
+	move_direction.E: Vector2i(1, 0),
+	move_direction.W: Vector2i(-1, 0),
+}
+
 
 ## bounds.x = min & bounds.y = max
 func run(start_cell : Vector2i, max_steps : int, start_dir : move_direction, bounds_min : Vector2i, bounds_max : Vector2i) -> Array[Vector2i]:
 	_bounds_min = bounds_min
 	_bounds_max = bounds_max
 
-	var walked : Array[Vector2i] = []
+	var tunneled : Array[Vector2i] = []
 
 	position = start_cell
-	walked.append(position)
+	tunneled.append(position)
 	
 	var rng = RandomNumberGenerator.new()
 
@@ -28,39 +36,53 @@ func run(start_cell : Vector2i, max_steps : int, start_dir : move_direction, bou
 		var cell_w = position + Vector2i(-1, 0)
 		var cells_in_directions = [cell_n, cell_s, cell_e, cell_w]
 
-		var inaccessible = []
-		for cell : Vector2i in cells_in_directions:
-			if not inside_bounds(cell):
-				inaccessible.append(cell)
+		var possible_directions = [
+			move_direction.N,
+			move_direction.S,
+			move_direction.E,
+			move_direction.W,
+		]
+
+		var inaccessible_directions = []
+		for dir : move_direction in possible_directions:
+			var offset : Vector2i = directions[dir]
+			var cell_in_dir = position + offset
+			if not inside_bounds(cell_in_dir):
+				inaccessible_directions.append(dir)
 				continue
 			# prevent walking over the same path twice - ? ? optional ? 
-			if walked.has(cell):
-				inaccessible.append(cell)
+			if tunneled.has(cell_in_dir):
+				inaccessible_directions.append(dir)
 				continue
 
-		for c in inaccessible:
-			cells_in_directions.erase(c)
+		for dir in inaccessible_directions:
+			possible_directions.erase(dir)
 
 		# nowhere to go anymore
-		if cells_in_directions.is_empty():
-			return walked
+		if possible_directions.is_empty():
+			print_debug("tunneling ended prematurely")
+			break
 		
 		# move
-		var new_pos
+		var move_dir : move_direction
 
 		if i == 0: # first step  -> try to move in user-requested direction
-			var cell_in_requested_start_direction = get_cell_in_dir(start_dir)
+			var cell_in_requested_start_direction = position + directions[start_dir]
 			if inside_bounds(cell_in_requested_start_direction):
-				new_pos = cell_in_requested_start_direction
+				move_dir = start_dir
 			else: 
-				return []
+				print_debug("cell in requested start direction is inaccessible")
+				print_debug("tunneling ended prematurely")
+				break
 		else:
-			new_pos = cells_in_directions.pick_random()
+			move_dir = possible_directions.pick_random()			
 		
-		position = new_pos
-		walked.append(position)
+		position = position + directions[move_dir]
+		tunneled.append(position)
+		print("Tunneled in direction: %s" % [move_direction.keys()[move_dir]])
 	
-	return walked
+	print("tunneler  tunneled %s steps out of %s requested." % [tunneled.size()-1, max_steps])
+	return tunneled
 
 func get_cell_in_dir(direction : move_direction) -> Vector2i:
 	match direction:
